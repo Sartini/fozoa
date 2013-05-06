fa8a5f40e857dd82073956742509c01060258521
ddsilva <daniel.silva@dclick.com.br>
2013-05-06 17:59:04 -0300

    Configuração do Grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,15 +2,14 @@
 module.exports = function(grunt) {
   var utils;
   utils = (require('./gruntcomponents/misc/commonutils'))(grunt);
+  
   grunt.task.loadTasks('gruntcomponents/tasks');
   grunt.task.loadNpmTasks('grunt-contrib-watch');
   grunt.task.loadNpmTasks('grunt-contrib-less');
+  
   grunt.initConfig({
     distdir: 'wp-content/themes/fozoa',
     pkg: grunt.file.readJSON('package.json'),
-    src: {
-      less: ['wp-content/themes/fozoa/*.less'],
-    },
 
     growl: {
       ok: {
@@ -20,52 +19,42 @@ module.exports = function(grunt) {
     },
 
     coffee: {
-      dist2: {
-        dir: 'wp-content/themes/fozoa/js',
-        dest: 'wp-content/themes/fozoa/js'
-      }, 
-      dist3: {
-        dir: 'js/',
-        dest: 'js/'
-      } 
+      compile: {
+        dir: '<%=distdir%>/js',
+        dest: '<%=distdir%>/js'
+      }
     },
 
     less: {
-      caminho: {
-        options : {yuicompress: true},
-        src: [
-          'wp-content/themes/fozoa/css/less/style-default.less',
-          'wp-content/themes/fozoa/css/less/style-pages.less'
-        ],
-        dest: 'wp-content/themes/fozoa/css/styleall.css'
+      theme: {
+        options : { yuicompress: true },
+        src: '<%=distdir%>/css/less/style.less',
+        dest: '<%=distdir%>/css/styleall.css'
       }, 
       styleIpad: {
-        //options : {yuicompress: true},
-        src:  'wp-content/themes/fozoa/css/less/style-768.less', 
-        dest: 'wp-content/themes/fozoa/css/style-768.css'
+        // options : { yuicompress: true },
+        src:  '<%=distdir%>/css/less/style-768.less', 
+        dest: '<%=distdir%>/css/style-768.css'
       }, 
       style980: {
-        //options : {yuicompress: true},
-        src:  'wp-content/themes/fozoa/css/less/style-980.less', 
-        dest: 'wp-content/themes/fozoa/css/style-980.css'
+        // options : { yuicompress: true },
+        src:  '<%=distdir%>/css/less/style-980.less', 
+        dest: '<%=distdir%>/css/style-980.css'
       }, 
       bootstrap: {
-        src:  'wp-content/themes/fozoa/library/less/bootstrap.less', 
-        dest: 'wp-content/themes/fozoa/library/css/bootstrap.css'
+        // options : { yuicompress: true },
+        src:  '<%=distdir%>/library/less/bootstrap.less', 
+        dest: '<%=distdir%>/library/css/bootstrap.css'
       }
        
     },
     watch: {
-      dist2: {
-        files: 'wp-content/themes/fozoa/js/*.coffee',
-        tasks: ['coffee:dist2', 'growl:ok']
+      coffee: {
+        files: '<%=distdir%>/js/**/*.coffee',
+        tasks: ['coffee', 'growl:ok']
       },  
-      dist3: {
-        files: 'js/*.coffee',
-        tasks: ['coffee:dist3', 'growl:ok']
-      }, 
       less: {
-        files: ['wp-content/themes/fozoa/css/less/*.less', 'wp-content/themes/fozoa/library/less/*.less' ],
+        files: ['<%=distdir%>/css/less/**/*.less', 'wp-content/themes/fozoa/library/less/**/*.less' ],
         tasks: 'less'
       } 
     }
