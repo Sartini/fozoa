7f5c8e06d6da81b8978adb119480f446ff86eba8
ddsilva <daniel.silva@dclick.com.br>
2013-05-06 17:02:27 -0300

    Refactoring

--- wp-content/themes/fozoa/page-quem-somos.php
+++ wp-content/themes/fozoa/page-quem-somos.php
@@ -4,89 +4,66 @@ Template Name: Quem Somos
 */
 ?>
 <?php get_header(); ?>
-	 
-			<div id="content" class="clearfix row-fluid">
-			
-				<div id="main" class="clearfix" role="main">
-
-					<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-					
-					<article id="post-<?php the_ID(); ?>" <?php post_class('clearfix'); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
-						
-						<header>
-							<?php 
-							  $src = wp_get_attachment_image_src( get_post_thumbnail_id(), 'full' );
-							  $src_url = get_bloginfo('template_directory') . '/timthumb.php?src='. $src[0] . '&w=1313&h=207&zc=0'; 
-							 ?>
-							<div class="page-header"><h1 class="page-title" itemprop="headline"><img src="<?php echo $src_url; ?>" alt=""></div>
-
-						
-						</header> <!-- end article header -->
-					
-						<section class="post_content clearfix" itemprop="articleBody">
-							
-
-							<!-- menu latera  -->
-							<div class="menulateral">
-								MENU LATERAL
-							</div>
-							
-							<div class="lateraldireito">
-
-
-								<!-- conteúdo da pagina -->
-								<div class="titulo">
-									<h1><?php the_title(); ?></h1>
-									<?php $codigo_da_galeria = get_post_meta($post->ID, 'codigo_da_galeria', true); ?>  
-								</div>
-								<div class="galeria">
-									<?php if ( $codigo_da_galeria > 0) : ?>
-										<?php do_action('slideshow_deploy', $codigo_da_galeria); ?> 
-									<?php endif; ?>
-								</div>
-								<div class="pagecontenta">
-									<p>
-									 <?php the_content(); ?>
-									</p>
-								</div>
-
-							</div>	
-								
-							
-					
-						</section> <!-- end article section -->
-						
-						<footer>
-			
-							<?php the_tags('<p class="tags"><span class="tags-title">' . __("Tags","bonestheme") . ':</span> ', ', ', '</p>'); ?>
-							
-						</footer> <!-- end article footer -->
-					
-					</article> <!-- end article -->
-					
-					 
-					
-					<?php endwhile; ?>		
-					
-					<?php else : ?>
-					
-					<article id="post-not-found">
-					    <header>
-					    	<h1><?php _e("Not Found", "bonestheme"); ?></h1>
-					    </header>
-					    <section class="post_content">
-					    	<p><?php _e("Sorry, but the requested resource was not found on this site.", "bonestheme"); ?></p>
-					    </section>
-					    <footer>
-					    </footer>
-					</article>
-					
-					<?php endif; ?>
-			
-				</div> <!-- end #main -->
+   
+<div id="content" class="clearfix row-fluid">
+  <div id="main" class="clearfix" role="main">
+    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+      <article id="post-<?php the_ID(); ?>" <?php post_class('clearfix'); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
+        <header>
+          <?php 
+            $src = wp_get_attachment_image_src( get_post_thumbnail_id(), 'full' );
+            $src_url = get_bloginfo('template_directory') . '/timthumb.php?src='. $src[0] . '&w=1313&h=207&zc=0'; 
+           ?>
+          <div class="page-header"><h1 class="page-title" itemprop="headline"><img src="<?php echo $src_url; ?>" alt=""></h1></div>        
+        </header> <!-- end article header -->
+      
+        <section class="post_content clearfix" itemprop="articleBody">
+          <!-- menu latera  -->
+          <div class="menulateral">
+            MENU LATERAL
+          </div>
+          
+          <div class="lateraldireito">
+            <!-- conteúdo da pagina -->
+            <div class="titulo">
+              <h1><?php the_title(); ?></h1>
+              <?php $codigo_da_galeria = get_post_meta($post->ID, 'codigo_da_galeria', true); ?>  
+            </div>
+            <div class="galeria">
+              <?php if ( $codigo_da_galeria > 0) : ?>
+                <?php do_action('slideshow_deploy', $codigo_da_galeria); ?> 
+              <?php endif; ?>
+            </div>
+            <div class="pagecontenta">
+              <p>
+               <?php the_content(); ?>
+              </p>
+            </div>
+          </div>  
+        </section> <!-- end article section -->
+      
+      </article> <!-- end article -->
     
-				 
+    <?php endwhile; ?>    
+    <?php else : ?>
     
-			</div> <!-- end #content -->
+      <article id="post-not-found">
+        <header>
+          <h1><?php _e("Not Found", "bonestheme"); ?></h1>
+        </header>
+
+        <section class="post_content">
+          <p><?php _e("Sorry, but the requested resource was not found on this site.", "bonestheme"); ?></p>
+        </section>
+      </article>
+    
+    <?php endif; ?>
+    
+    <footer>
+      <?php the_tags('<p class="tags"><span class="tags-title">' . __("Tags","bonestheme") . ':</span> ', ', ', '</p>'); ?>
+    </footer> <!-- end article footer -->
+
+  </div> <!-- end #main -->  
+</div> <!-- end #content -->
 
 <?php get_footer(); ?>
